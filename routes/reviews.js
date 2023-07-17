6bd16194f2454a9ca1448b139e271e5c32facba9
VedantPanda <ved07panda@gmail.com>
2023-07-18 00:32:46 +0530

    restructured reviews route using express router

---
+++ routes/reviews.js
@@ -0,0 +1,38 @@
+const express= require('express');
+const router = express.Router({mergeParams:true});
+const catchAsync = require('../utils/catchAsync');
+const ExpressError = require("../utils/ExpressError");
+const Campground = require('../models/campGrounds');
+const Review = require('../models/reviews');
+const {reviewSchema} = require('../validateSchemas');
+
+const validateReview = (req,res,next) => {
+    const{error} = reviewSchema.validate(req.body);
+    if(error){
+        const msg = error.details.map(el=>el.message).join(",");
+        throw new ExpressError(msg,404);
+    }
+    else{
+        next();
+    }
+}
+
+router.post("/", validateReview, catchAsync(async(req,res)=>{
+    const{id} = req.params;
+    const review = new Review(req.body.review);
+    const campground = await Campground.findById(id);
+    campground.reviews.push(review);
+    await campground.save();
+    await review.save();
+    res.redirect(`/campgrounds/show/${campground.id}`);
+}))
+
+router.delete("/:reviewId",catchAsync(async(req,res)=>{
+    const campgroundId = req.params.id;
+    const {reviewId} = req.params;
+    await Campground.findByIdAndUpdate(campgroundId,{$pull:{reviews:reviewId}});
+    await Review.findByIdAndDelete(reviewId);
+    res.redirect(`/campgrounds/show/${campgroundId}`);
+}))
+
+module.exports = router;
