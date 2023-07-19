dfd70122ca685fb29f743ca88692737ca795904b
VedantPanda <ved07panda@gmail.com>
2023-07-19 18:00:49 +0530

    Added middleware for flash messages

--- app.js
+++ app.js
@@ -3,14 +3,10 @@ const app = express();
 const port = 3000;
 const path = require('path');
 const mongoose = require('mongoose');
-const Campground = require('./models/campGrounds');
-const {reviewSchema} = require('./validateSchemas');
-const catchAsync = require('./utils/catchAsync');
 const ExpressError = require("./utils/ExpressError");
 const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
 const ejsMate = require('ejs-mate');
-const Review = require('./models/reviews');
 const campgroundRoutes = require('./routes/campgrounds');
 const reviewRoutes = require('./routes/reviews');
 const session = require('express-session');
@@ -37,9 +33,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname,'public')));
 
-app.use("/campgrounds",campgroundRoutes);
-app.use("/campgrounds/:id/review",reviewRoutes);
-
 const configSession = {
     secret:"secret-key",
     resave:false,
@@ -53,6 +46,15 @@ const configSession = {
 
 app.use(session(configSession));
 app.use(flash());
+
+app.use((req,res,next)=>{
+    res.locals.success = req.flash('success');
+    res.locals.error = req.flash('error');
+    next();
+})
+
+app.use("/campgrounds",campgroundRoutes);
+app.use("/campgrounds/:id/review",reviewRoutes);
 
 const validateReview = (req,res,next) => {
     const{error} = reviewSchema.validate(req.body);
