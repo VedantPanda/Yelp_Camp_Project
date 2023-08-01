485abb80b9ed922b92a425af647be3b8bd7391ac
VedantPanda <ved07panda@gmail.com>
2023-08-01 22:15:19 +0530

    Minor changes

--- app.js
+++ app.js
@@ -66,12 +66,6 @@ app.use((req,res,next)=>{
     next();
 })
 
-app.get("/fakeUser",async(req,res)=>{
-    const user = new User({email:'colttt@gmail.com',username:'colttt'});
-    const newUser = await User.register(user,'chicken');
-    res.send(newUser);
-})
-
 app.use("/",userRoutes);
 app.use("/campgrounds",campgroundRoutes);
 app.use("/campgrounds/:id/review",reviewRoutes);
