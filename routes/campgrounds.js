dfb130d4a02facc325723b2cf94f7b9083dfbc60
VedantPanda <ved07panda@gmail.com>
2023-08-12 11:24:33 +0530

    Cleared unnecessary imports

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const catchAsync = require('../utils/catchAsync');
-const Campground = require('../models/campGrounds');
 const {validateCampground} = require('../middleware');
 const {isLoggedIn,isAuthorized} = require('../middleware');
 const campgrounds = require('../controllers/campgrounds');
--- routes/reviews.js
+++ routes/reviews.js
@@ -1,9 +1,6 @@
 const express= require('express');
 const router = express.Router({mergeParams:true});
 const catchAsync = require('../utils/catchAsync');
-const ExpressError = require("../utils/ExpressError");
-const Campground = require('../models/campGrounds');
-const Review = require('../models/reviews');
 const {validateReview} = require('../middleware');
 const {isLoggedIn,isReviewAuthorized} = require('../middleware');
 const reviews = require('../controllers/reviews');
